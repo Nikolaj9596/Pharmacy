0fb43308e804400b33117e36cd69ca1bda476dc2
Nikolaj <nikolaj.v@itfox-web.com>
2023-12-19 12:15:55 +0300

    feat: :sparkles: added new constait for field name

--- src/profession_app/models.py
+++ src/profession_app/models.py
@@ -1,4 +1,4 @@
-__all__ = ['Profession']
+__all__ = ['ProfessionORM']
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from src.models import Base
@@ -6,5 +6,5 @@ from src.models import Base
 class ProfessionORM(Base):
     __tablename__ = 'profession'
 
-    name: Mapped[str] = mapped_column(String(255))
+    name: Mapped[str] = mapped_column(String(255), unique=True)
     doctors: Mapped['Doctor'] = relationship(back_populates='profession')
