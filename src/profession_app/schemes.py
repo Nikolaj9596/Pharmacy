865e7b225e30c2fa1fe7bf8f3a1f6a17dffea527
Nikolaj <nikolaj.v@itfox-web.com>
2023-12-19 12:23:22 +0300

    feat: :sparkles: added new class ProfessionDetsilScheme

--- src/profession_app/schemes.py
+++ src/profession_app/schemes.py
@@ -9,3 +9,6 @@ class ProfessionScheme(ProfessionCreateScheme):
     id: int
     created_at: datetime
     updated_at: datetime
+
+class ProfessionDetailScheme(ProfessionScheme):
+    number_of_specialists: int
