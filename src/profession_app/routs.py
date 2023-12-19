eb88f9c362d525e23183bf271ed8c5cb8f686d49
Nikolaj <nikolaj.v@itfox-web.com>
2023-12-19 12:24:13 +0300

    feat: :sparkles: finished work with routs profession

--- src/profession_app/routs.py
+++ src/profession_app/routs.py
@@ -1,15 +1,33 @@
 from typing import Annotated, Optional
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Response
 from fastapi.responses import JSONResponse
 from sqlalchemy.ext.asyncio import AsyncSession
-from src.dependencies import get_session
+from src.dependencies import Paginator, QueryParams, get_session
 from src.profession_app.dependencies import profession_service
 
-from src.profession_app.schemes import ProfessionScheme
+from src.profession_app.schemes import ProfessionCreateScheme, ProfessionScheme
 from src.profession_app.services import ProfessionService
 
 
 profession_api = APIRouter(prefix='/professions')
+
+
+@profession_api.get(
+    '/',
+    tags=['Профессия'],
+    summary='Получить все профессии',
+    response_model=list[ProfessionScheme] | list,
+)
+async def get_list(
+    service: Annotated[ProfessionService, Depends(profession_service)],
+    session: Annotated[AsyncSession, Depends(get_session)],
+    pagination: Annotated[Paginator, Depends(Paginator)],
+    query_params: Annotated[QueryParams, Depends(QueryParams)]
+):
+    professions: list[ProfessionScheme] | list = await service.get_list(
+        session=session, pagination=pagination, query_params=query_params
+    )
+    return professions
 
 
 @profession_api.get(
@@ -18,20 +36,62 @@ profession_api = APIRouter(prefix='/professions')
     summary='Получить информацию о профессии',
     response_model=ProfessionScheme,
 )
-async def get_profession(
+async def get_retrieve(
     profession_id: int,
     service: Annotated[ProfessionService, Depends(profession_service)],
     session: Annotated[AsyncSession, Depends(get_session)],
 ):
-    profession: Optional[ProfessionScheme] = await service.get_by_id(
+    profession: ProfessionScheme = await service.get_by_id(
         id=profession_id, session=session
     )
-    print(f'{profession=}')
-    if not profession:
-        return JSONResponse(
-            status_code=404,
-            content={
-                'detail': f'Profession with id: {profession_id} not found'
-            },
-        )
-    return JSONResponse(status_code=200, content=profession)
+    return profession
+
+
+@profession_api.post(
+    '/',
+    tags=['Профессия'],
+    summary='Создать профессию',
+    response_model=ProfessionScheme,
+)
+async def create(
+    service: Annotated[ProfessionService, Depends(profession_service)],
+    session: Annotated[AsyncSession, Depends(get_session)],
+    data: ProfessionCreateScheme,
+):
+    professions: ProfessionScheme = await service.create(
+        session=session, data=data
+    )
+    return professions
+
+
+@profession_api.patch(
+    '/{profession_id}',
+    tags=['Профессия'],
+    summary='Обновление профессии',
+    response_model=ProfessionScheme,
+)
+async def update(
+    profession_id: int,
+    service: Annotated[ProfessionService, Depends(profession_service)],
+    session: Annotated[AsyncSession, Depends(get_session)],
+    data: ProfessionCreateScheme,
+):
+    profession: Optional[ProfessionScheme] = await service.update(
+        id=profession_id, session=session, data=data
+    )
+    return profession
+
+@profession_api.delete(
+    '/{profession_id}',
+    tags=['Профессия'],
+    summary='Удалить профессию',
+)
+async def delete(
+    profession_id: int,
+    service: Annotated[ProfessionService, Depends(profession_service)],
+    session: Annotated[AsyncSession, Depends(get_session)],
+):
+    await service.delete(
+        id=profession_id, session=session
+    )
+    return Response(status_code=204, content=None)
--- src/profession_app/services.py
+++ src/profession_app/services.py
@@ -1,21 +1,95 @@
-from typing import Optional
+from typing import NoReturn, Optional
+from sqlalchemy.exc import IntegrityError
 
 from sqlalchemy.ext.asyncio import AsyncSession
-from src.profession_app.dtos import ProfessionDataGet
+from src.dependencies import Paginator, QueryParams
+from src.exeptions import BadRequestEx, NotFoundEx
+from src.profession_app.dtos import (
+    ProfessionDataCreate,
+    ProfessionDataDetailGet,
+    ProfessionDataGet,
+)
 from src.profession_app.repositories import IProfessionRepository
-from src.profession_app.schemes import ProfessionScheme
+from src.profession_app.schemes import (
+    ProfessionCreateScheme,
+    ProfessionDetailScheme,
+    ProfessionScheme,
+)
 
 
 class ProfessionService:
     def __init__(self, repository: IProfessionRepository):
         self.repository = repository
 
-    async def get_by_id(
+    async def _check_exists(
         self, id: int, session: AsyncSession
-    ) -> Optional[ProfessionScheme]:
+    ) -> ProfessionDataGet | NoReturn:
         profession: Optional[
             ProfessionDataGet
         ] = await self.repository.get_by_id(id=id, session=session)
         if not profession:
-            return None
+            raise NotFoundEx(detail=f'Profession with id: {id} not found')
+        return profession
+
+    async def get_by_id(
+        self, id: int, session: AsyncSession
+    ) -> ProfessionScheme:
+        profession = await self._check_exists(id=id, session=session)
         return ProfessionScheme(**profession)
+
+    async def get_list(
+        self,
+        session: AsyncSession,
+        pagination: Paginator,
+        query_params: QueryParams,
+    ) -> list[ProfessionScheme] | list:
+        professions: list[
+            ProfessionDataDetailGet
+        ] | list = await self.repository.get_list(
+            session=session, 
+            limit=pagination.limit, 
+            offset=pagination.offset,
+            query_params=query_params
+        )
+        if not professions:
+            return professions
+        return [
+            ProfessionDetailScheme(**profession) for profession in professions
+        ]
+
+    async def create(
+        self, session: AsyncSession, data: ProfessionCreateScheme
+    ) -> ProfessionScheme:
+        try:
+            profession: ProfessionDataGet = await self.repository.create(
+                session=session, data=ProfessionDataCreate(**data.model_dump())
+            )
+        except IntegrityError:
+            raise BadRequestEx(
+                detail='There is already a profession with this name'
+            )
+        return ProfessionScheme(**profession)
+
+    async def update(
+        self, session: AsyncSession, data: ProfessionCreateScheme, id: int
+    ) -> ProfessionScheme:
+        profession = await self._check_exists(id=id, session=session)
+
+        try:
+            profession = await self.repository.update(
+                session=session,
+                data=ProfessionDataCreate(**data.model_dump()),
+                id=id,
+            )
+        except IntegrityError:
+            raise BadRequestEx(
+                detail='There is already a profession with this name'
+            )
+        return ProfessionScheme(**profession)
+
+    async def delete(self, id: int, session: AsyncSession) -> None:
+        await self._check_exists(id=id, session=session)
+        deleted = await self.repository.delete(session=session, id=id)
+        if not deleted:
+            raise BadRequestEx(detail='Failed to delete a profession')
+        return None
