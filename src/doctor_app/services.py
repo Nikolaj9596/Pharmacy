2486fee77e3df82eb0471eb767fc1109ea922468
Nikolaj <nikolaj.v@itfox-web.com>
2023-12-20 14:03:16 +0300

    feat: added AppointmentService

--- src/doctor_app/services.py
+++ src/doctor_app/services.py
@@ -1,11 +1,26 @@
+__all__ = ['AppointmentService', 'DoctorService']
+
 from typing import Optional
 from sqlalchemy import text
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio.session import AsyncSession
-from src.dependencies import Paginator, QueryParams
-from src.doctor_app.dtos import DoctorData, DoctorDataCreate, DoctorDetailData
-from src.doctor_app.repositories import IDoctorRepository
+from src.dependencies import Paginator, QueryParams, QueryParamsAppointment
+from src.doctor_app.dtos import (
+    AppointmentData,
+    AppointmentDataCreate,
+    AppointmentDetailData,
+    DoctorData,
+    DoctorDataCreate,
+    DoctorDetailData,
+)
+from src.doctor_app.repositories import (
+    IDoctorAppointmentRepository,
+    IDoctorRepository,
+)
 from src.doctor_app.schemes import (
+    AppointmentCreateScheme,
+    AppointmentDetailScheme,
+    AppointmentScheme,
     DoctorCreateScheme,
     DoctorDetailScheme,
     DoctorScheme,
@@ -103,4 +118,132 @@ class DoctorService:
         deleted = await self.repository.delete(session=session, id=id)
         if not deleted:
             raise BadRequestEx(detail='Failed to delete a profession')
+        return None
+
+
+class AppointmentService:
+    def __init__(self, repository: IDoctorAppointmentRepository):
+        self.repository = repository
+
+    async def _check_exists(
+        self, id: int, session: AsyncSession
+    ) -> AppointmentData:
+        doctor: Optional[AppointmentData] = await self.repository.get_by_id(
+            id=id, session=session
+        )
+        if not doctor:
+            raise NotFoundEx(detail=f'Appointment with id: {id} not found')
+        return doctor
+
+    async def _check_related_doctor_exists(
+        self, id: int, session: AsyncSession
+    ) -> bool:
+        query = text('SELECT d.id FROM doctor d WHERE d.id=:id')
+        result = await session.execute(query, {'id': id})
+        row = result.first()
+        if not row:
+            raise NotFoundEx(detail=f'Doctor with id: {id} not found')
+        return True
+
+    async def _check_related_client_exists(
+        self, id: int, session: AsyncSession
+    ) -> bool:
+        query = text('SELECT c.id FROM client c WHERE c.id=:id')
+        result = await session.execute(query, {'id': id})
+        row = result.first()
+        if not row:
+            raise NotFoundEx(detail=f'Client with id: {id} not found')
+        return True
+
+    async def _check_doctor_is_busy(
+        self,
+        data: AppointmentCreateScheme,
+        session: AsyncSession,
+    ) -> bool:
+        appointments: list[
+            AppointmentData
+        ] | list = await self.repository.get_list(
+            session=session,
+            limit=1,
+            offset=0,
+            query_params=QueryParamsAppointment(
+                start_date=data.start_date_appointment,
+                end_date=data.end_date_appointment,
+                doctor=data.doctor_id,
+                client=data.client_id,
+            ),
+        )
+        if appointments:
+            return True
+        return False
+
+    async def get_by_id(
+        self, id: int, session: AsyncSession
+    ) -> AppointmentDetailScheme:
+        appointment: Optional[
+            AppointmentDetailData
+        ] = await self.repository.get_detail_by_id(id=id, session=session)
+        if not appointment:
+            raise NotFoundEx(detail=f'Appointment with id: {id} not found')
+        return AppointmentDetailScheme(**appointment)
+
+    async def get_list(
+        self,
+        session: AsyncSession,
+        pagination: Paginator,
+        query_params: QueryParamsAppointment,
+    ) -> list[AppointmentScheme] | list:
+        appointments: list[
+            AppointmentData
+        ] | list = await self.repository.get_list(
+            session=session,
+            limit=pagination.limit,
+            offset=pagination.offset,
+            query_params=query_params,
+        )
+        if not appointments:
+            return appointments
+        return [
+            AppointmentScheme(**appointment) for appointment in appointments
+        ]
+
+    async def create(
+        self, session: AsyncSession, data: AppointmentCreateScheme
+    ) -> AppointmentScheme:
+        await self._check_related_doctor_exists(
+            id=data.doctor_id, session=session
+        )
+        await self._check_related_client_exists(
+            id=data.client_id, session=session
+        )
+        await self._check_doctor_is_busy(session=session, data=data)
+
+        appointment: AppointmentData = await self.repository.create(
+            session=session, data=AppointmentDataCreate(**data.model_dump())
+        )
+        return AppointmentScheme(**appointment)
+
+    async def update(
+        self, session: AsyncSession, data: AppointmentCreateScheme, id: int
+    ) -> AppointmentScheme:
+        await self._check_exists(id=id, session=session)
+        await self._check_related_doctor_exists(
+            id=data.doctor_id, session=session
+        )
+        await self._check_related_client_exists(
+            id=data.client_id, session=session
+        )
+        await self._check_doctor_is_busy(session=session, data=data)
+        appointment = await self.repository.update(
+            session=session,
+            data=AppointmentDataCreate(**data.model_dump()),
+            id=id,
+        )
+        return AppointmentScheme(**appointment)
+
+    async def delete(self, id: int, session: AsyncSession) -> None:
+        await self._check_exists(id=id, session=session)
+        deleted = await self.repository.delete(session=session, id=id)
+        if not deleted:
+            raise BadRequestEx(detail='Failed to delete a appointment')
         return None
