fa0835f894a781fce513ae65bb3b8bddc3fbb812
Nikolaj <nikolaj.v@itfox-web.com>
2023-12-20 16:31:01 +0300

    feat: improved Client model

--- src/client_app/models.py
+++ src/client_app/models.py
@@ -1,36 +1,25 @@
 from datetime import date
 
-from sqlalchemy import Date, ForeignKey, String
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import UniqueConstraint
+from sqlalchemy.orm import Mapped, relationship
 
-from src.models import Base
-
-
-class Client(Base):
-    __tablename__ = "client"
-
-    first_name: Mapped[str] = mapped_column(String(255))
-    last_name: Mapped[str] = mapped_column(String(255))
-    middle_name: Mapped[str] = mapped_column(String(255))
-    date_birthday: Mapped[date] = mapped_column(Date())
-    address: Mapped[str] = mapped_column(String(255))
-    appointments: Mapped["DoctorAppointment"] = relationship(back_populates="client")
-    client_diagnosis: Mapped["ClientDiagnosis"] = relationship(back_populates="client")
+from src.models import BaseUser, str_255
 
 
-class ClientDiagnosis(Base):
-    __tablename__ = "client_diagnosis"
+class Client(BaseUser):
+    __tablename__ = 'client'
 
-    diagnosis_id: Mapped[int] = mapped_column(
-        ForeignKey("diagnosis.id", ondelete="CASCADE"), index=True
+    __table_args__ = (
+        UniqueConstraint(
+            'first_name',
+            'last_name',
+            'middle_name',
+            name='uq_client_full_name',
+        ),
     )
-    client_id: Mapped[int] = mapped_column(
-        ForeignKey("client.id", ondelete="CASCADE"), index=True
+    date_birthday: Mapped[date]
+    address: Mapped[str_255]
+    appointments: Mapped['DoctorAppointment'] = relationship(
+        back_populates='client'
     )
-    doctor_id: Mapped[int] = mapped_column(
-        ForeignKey("doctor.id", ondelete="CASCADE"), index=True
-    )
-
-    diagnosis: Mapped["Diagnosis"] = relationship(back_populates="client_diagnosis")
-    client: Mapped["Client"] = relationship(back_populates="client_diagnosis")
-    doctor: Mapped["Doctor"] = relationship(back_populates="client_diagnosis")
+    diagnosis: Mapped['Diagnosis'] = relationship(back_populates='client')
