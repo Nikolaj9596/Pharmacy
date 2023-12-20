af99a91cacc3f02f8cbb466757e790e2e44dda88
Nikolaj <nikolaj.v@itfox-web.com>
2023-12-20 16:32:09 +0300

    feat: added client schemes

---
+++ src/client_app/schemes.py
@@ -0,0 +1,19 @@
+from datetime import date, datetime
+from typing import Annotated
+from pydantic import BaseModel
+
+from src.types import POS_INT, STR_255, STR_50
+
+
+class ClientCreateScheme(BaseModel):
+    first_name: Annotated[str, STR_50]
+    last_name: Annotated[str, STR_50]
+    middle_name: Annotated[str, STR_50]
+    date_birthday: date
+    address: Annotated[str, STR_255]
+
+
+class ClientScheme(ClientCreateScheme):
+    id: Annotated[int, POS_INT]
+    created_at: datetime
+    updated_at: datetime
