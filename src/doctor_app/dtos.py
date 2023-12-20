d0d2d31de9b77942d8d537b146555cbfe26beecc
Nikolaj <nikolaj.v@itfox-web.com>
2023-12-20 14:02:00 +0300

    feat: added dtos for appointment

--- src/doctor_app/dtos.py
+++ src/doctor_app/dtos.py
@@ -1,11 +1,11 @@
-from datetime import datetime
+from datetime import date, datetime
 from typing import TypedDict
 
 class DoctorDataCreate(TypedDict):
     first_name: str
     last_name: str
     middle_name: str
-    date_start_work: datetime
+    date_start_work: date
     profession: int
 
 class DoctorData(DoctorDataCreate):
@@ -19,3 +19,37 @@ class DoctorProfession(TypedDict):
 
 class DoctorDetailData(DoctorData):
     profession: DoctorProfession
+
+class AppointmentDataCreate(TypedDict):
+    start_date_appointment: datetime
+    end_date_appointment: datetime
+    doctor_id: int
+    client_id: int
+
+class AppointmentData(AppointmentDataCreate):
+    id: int 
+    created_at: datetime
+    updated_at: datetime
+    
+class DoctorAppointmentInfo(TypedDict):
+    id: int
+    first_name: str
+    last_name: str
+    middle_name: str
+
+class ClientAppointmentInfo(TypedDict):
+    id: int
+    first_name: str
+    last_name: str
+    middle_name: str
+
+class AppointmentDetailData(TypedDict):
+    id: int 
+    created_at: datetime
+    updated_at: datetime
+    start_date_appointment: datetime
+    end_date_appointment: datetime
+    doctor: DoctorAppointmentInfo
+    client: ClientAppointmentInfo
+
+    
