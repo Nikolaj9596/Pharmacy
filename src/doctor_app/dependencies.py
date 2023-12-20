36e8c467c1d2e743e29fd974f10f407b96c9f58a
Nikolaj <nikolaj.v@itfox-web.com>
2023-12-20 14:01:35 +0300

    feat: added appointment_service

--- src/doctor_app/dependencies.py
+++ src/doctor_app/dependencies.py
@@ -1,6 +1,11 @@
-from src.doctor_app.repositories import DoctorRepository
-from src.doctor_app.services import DoctorService
+from datetime import datetime
+from typing import Optional
+from src.doctor_app.repositories import DoctorRepository, DoctorAppointmentRepository
+from src.doctor_app.services import AppointmentService, DoctorService
 
 
 def doctor_service() -> DoctorService:
     return DoctorService(repository=DoctorRepository())
+
+def appointment_service() -> AppointmentService:
+    return AppointmentService(repository=DoctorAppointmentRepository())
